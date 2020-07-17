aa76e5ec22196bd097ec435d83d861b5dec24140
Megan Skelton <megan.skelton@kcl.ac.uk>
2020-07-17 13:59:12 +0100

    biscuits means in belly groups

--- File_info.R
+++ File_info.R
@@ -1,4 +1,6 @@
 # Assign your downloaded datapath to the variable below
+# Right click - get infoand where OR right click with alt and copy as file path name
+# Obviously normally we would save this file outside of the folder we're working in to keep the file paths anonymised - do not save this file in the git repository!
 
-datapath <- "path/to/data"
+datapath <- "/Users/megskelton/Desktop/CollabGit_Data/nutrition-studies"
   
