a0014e1c8d6c491543f5c83c768d0cffe11676ba
Kirstin Purves <kirstin.purves@kcl.ac.uk>
2020-07-17 17:18:39 +0100

    added markdown with upstream command fixed

--- File_info.R
+++ File_info.R
@@ -1,5 +1,5 @@
 # Assign your downloaded datapath to the variable below
 
-datapath <- "/Users/aliciapeel/Desktop/nutrition-studies/raw_anonymized_data.csv"
+datapath <- "path/to/data"
   
 ##don't save in same git repository, the file path should be secret
