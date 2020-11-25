94b84e111930f4fe8fffa37db2db6ab3580627da
Helena-D <helena.davies@kcl.ac.uk>
2020-11-25 14:39:51 +0000

    added correlation between currently smoking and owning a cat

--- File_info.R
+++ File_info.R
@@ -1,5 +1,5 @@
 # Assign your downloaded datapath to the variable below
 
-datapath <- "path/to/data"
+datapath <- "/Users/helenadavies/Desktop/Git_data2/nutrition-studies/raw_anonymized_data.csv"
   
 ##don't save in same git repository, the file path should be secret
