f37e3e4cdd0e1b5e912f545bf1dacce208217427
Quinn Connolly-Smith <quinn.connolly-smith@kcl.ac.uk>
2020-11-25 15:49:28 +0000

    updated file info

--- File_info.R
+++ File_info.R
@@ -1,5 +1,5 @@
 # Assign your downloaded datapath to the variable below
 
-datapath <- "path/to/data"
+datapath <- "C:/Users/Olivia Connolly/Documents/Uni/Placement/KCL/R/nutrition-studies/raw_anonymized_data.csv"
   
 ##don't save in same git repository, the file path should be secret
